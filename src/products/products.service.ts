d71f8a0dc32ee4eb372da2e594abbd0123118532
sebastian tovar <sebastiantovarchavez304@gmail.com>
2026-04-08 23:48:46 -0500

    feat: :sparkles: nest module products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -27,7 +27,6 @@ export class ProductsService {
     return product;
   }
   
-
   async update(id: number, updateProductDto: UpdateProductDto) {
     try {
       return await this.prismaService.product.update({
